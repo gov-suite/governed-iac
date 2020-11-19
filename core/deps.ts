e9e5047722ed77f587a915e6b2e121145c9ba4fb
divya-sk <divya.kumar@citrusinformatics.com>
2020-11-19 10:57:32 +0530

    Update version changes

--- core/deps.ts
+++ core/deps.ts
@@ -1,9 +1,9 @@
-export * as testingAsserts from "https://deno.land/std@0.77.0/testing/asserts.ts";
-export * as encodingYAML from "https://deno.land/std@0.77.0/encoding/yaml.ts";
+export * as testingAsserts from "https://deno.land/std@0.78.0/testing/asserts.ts";
+export * as encodingYAML from "https://deno.land/std@0.78.0/encoding/yaml.ts";
 export * as documentArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.1.0/document/mod.ts";
 export * as artfPersist from "https://denopkg.com/shah/artifacts-persistence@v1.1.0/mod.ts";
 export * as polyglotArtfNature from "https://denopkg.com/shah/artifacts-persistence@v1.1.0/polyglot/mod.ts";
 export * as contextMgr from "https://denopkg.com/shah/context-manager@v1.0.6/mod.ts";
 export * as specModule from "https://denopkg.com/shah/specification-module@v1.0.6/mod.ts";
-export * as textInflect from "https://denopkg.com/shah/text-inflect@v1.0.4/mod.ts";
-export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.8/mod.ts";
+export * as textInflect from "https://denopkg.com/shah/text-inflect@v1.0.5/mod.ts";
+export * as valueMgr from "https://denopkg.com/shah/value-manager@v1.0.9/mod.ts";
