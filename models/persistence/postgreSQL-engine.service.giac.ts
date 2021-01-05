aafdb8e8a4856334ca8d59c9f3acd5e671a1570b
Jibin Joy A <jibin.joy@citrusinformatics.com>
2021-01-05 16:02:21 +0530

    Remove duplicate env variables

--- models/persistence/postgreSQL-engine.service.giac.ts
+++ models/persistence/postgreSQL-engine.service.giac.ts
@@ -79,10 +79,6 @@ export class PostgreSqlEngineServiceConfig
       "POSTGRESQLENGINE_PASSWORD",
       "Postgres user password",
     );
-    ctx.envVars.requiredEnvVar(
-      "POSTGRESQLENGINE_PUBL_PORT",
-      "Postgres database host port",
-    );
     this.ports = giac.portsFactory.publishSingle(
       ctx.envVars.defaultEnvVar(
         "PUBL_PORT",
